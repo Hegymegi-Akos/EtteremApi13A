3d1f09563e4d96dce23096a1e156ada91a8d7f5b
FZoltan40 <farkasz@kkszki.hu>
2025-12-12 08:23:25 +0100

    Initial commit.

---
+++ EtteremApi13A/Models/Rendele.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EtteremApi13A.Models;
+
+public partial class Rendele
+{
+    public int RendelesId { get; set; }
+
+    public int AsztalSzam { get; set; }
+
+    public string FizetesMod { get; set; } = null!;
+
+    public virtual ICollection<Rendelestetel> Rendelestetels { get; set; } = new List<Rendelestetel>();
+}
---
+++ EtteremApi13A/Models/Rendelestetel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EtteremApi13A.Models;
+
+public partial class Rendelestetel
+{
+    public int TetelId { get; set; }
+
+    public int RendelesId { get; set; }
+
+    public int TermekId { get; set; }
+
+    public virtual Rendele Rendeles { get; set; } = null!;
+
+    public virtual Termek Termek { get; set; } = null!;
+}
